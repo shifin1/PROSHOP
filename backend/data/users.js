93321ac93b0f336ee7c7a8239420f6d1b0332d74
Shifin <mohd.shifinshad@gmail.com>
2022-12-11 10:37:31 +0530

    Add sample data to db

---
+++ backend/data/users.js
@@ -0,0 +1,20 @@
+import bcrypt from "bcryptjs"
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hash("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "user1",
+    email: "user1@example.com",
+    password: bcrypt.hash("123456", 10),
+  },
+  {
+    name: "user2",
+    email: "user2@example.com",
+    password: bcrypt.hash("123456", 10),
+  },
+]
