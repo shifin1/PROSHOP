14c302052d0f4ff31f0be4290ad2c30c81c920f5
Shifin <mohd.shifinshad@gmail.com>
2022-12-13 15:07:33 +0530

    Product details reducer and action in product screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,14 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import { applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
-import { productListReducer } from "./reducers/productReducers"
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers"
 
 const allReducers = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 })
 
 const initialState = {}
