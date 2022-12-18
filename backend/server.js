05bd70d40e6fbbe1faee5749f41eec5bf4d8967c
Shifin <mohd.shifinshad@gmail.com>
2022-12-18 11:16:14 +0530

    user routes, user controller, and compare password

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,25 @@
 import express from "express"
 import * as dotenv from "dotenv"
+import bodyParser from "body-parser"
 import colors from "colors"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import connectDB from "./config/db.js"
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
   res.send("API is running.....")
 })
 
 app.use("/api/products", productRoutes)
+app.use("/api/users", userRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
