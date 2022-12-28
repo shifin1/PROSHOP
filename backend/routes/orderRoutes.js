714d7acf8e04012789dbf59986924be5f4e0df74
Shifin <mohd.shifinshad@gmail.com>
2022-12-28 08:14:15 +0530

    order controller and route changed errors

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
-import { addOrderItems } from "../controllers/orderController"
-import { protect } from "../middleware/authMiddleware"
+import { addOrderItems } from "../controllers/orderController.js"
+import { protect } from "../middleware/authMiddleware.js"
 
 const router = express.Router()
 
