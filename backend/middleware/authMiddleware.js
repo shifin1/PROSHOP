4970e83d7d07de804e591ed44687b664637d7389
Shifin <mohd.shifinshad@gmail.com>
2022-12-31 08:43:09 +0530

    admin middleware and get all users endpoint

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -28,5 +28,13 @@ const protect = asyncHandler(async (req, res, next) => {
     throw new Error("not authorized, no token")
   }
 })
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error("Not Authorized as an admin")
+  }
+}
 
-export { protect }
+export { protect, admin }
